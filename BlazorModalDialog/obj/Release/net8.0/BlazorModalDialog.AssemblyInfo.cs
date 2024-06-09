f1ff20d1a4eb58857d0f4a33f330d2543a03267b
Chris Schroll <schrollc@gmail.com>
2024-06-09 16:34:11 -0400

    Deployed as a NuGet package.

--- BlazorModalDialog/obj/Release/net8.0/BlazorModalDialog.AssemblyInfo.cs
+++ BlazorModalDialog/obj/Release/net8.0/BlazorModalDialog.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consultants R Us")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("This is a Razor component library with a cool modal window component.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0+e037300fa98e543dde0ce54e6f81bf0a07048153")]
